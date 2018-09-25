f0fa2dedbf3351170239c1adaa96ac566be76cd5
mrl5 <jakuboo+github@gmail.com>
2018-09-25 21:12:35 +0200

    true bash source code: functions, test-logic

--- split-pdf.sh
+++ split-pdf.sh
@@ -13,38 +13,53 @@
 # Example: 'sh split-pdf.sh /path/to/input.pdf /path/to/output.pdf 1-1 6-9 18-21'
 #  creates output.pdf from input.pdf pages: 1, 6, 7, 8, 9, 18, 19, 20, 21
 
-TMPNAME="temp-pdf-snapshot"
-TMPDIR="/tmp/temp-pdf-snapshots-$(tr -dc A-Za-z0-9_ < /dev/urandom | head -c 16 | xargs)"
+# checks if enough arguments were provided
+if [ $# -lt 3 ]; then
+        echo "Expected minimum 3 arguments. Please specify output file, input files and ranges."
+        exit 1
+else
+	TMPNAME="temp-pdf-snapshot"
+	TMPDIR="/tmp/temp-pdf-snapshots-$(tr -dc A-Za-z0-9_ < /dev/urandom | head -c 16 | xargs)"
+	# stores directory of the script
+	# source: https://stackoverflow.com/a/246128
+	SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null && pwd -P)"
+	# get directory of /path/to/outputfile.pdf file
+	OUTPUTDIR=${2%/*}
+	OUTPUT_FILE="$2"
+	INPUT_FILE="$1"
+	PAGES="${@:3}"
+fi
 
-# stores directory of the script
-# source: https://stackoverflow.com/a/246128
-SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null && pwd -P)"
-
-# get directory of /path/to/outputfile.pdf file
-OUTPUTDIR=${2%/*}
-
-if [ $OUTPUTDIR == $2 ]
+if [ $OUTPUTDIR == $OUTPUT_FILE ]
 then
     OUTPUTDIR="."
 fi
 
-mkdir $TMPDIR
-
-INDEX=1
-# start loop from 3rd argument
-for RANGE in ${@:3}
-do
-    START=$(echo $RANGE | cut -d'-' -f1)
-    END=$(echo $RANGE | cut -d'-' -f2)
-
-    # create new pdf from range $START-$END
-    #'$(printf "%04g" $INDEX)' = padding $INDEX with leading zeros
-    gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=$START -dLastPage=$END -sOutputFile=$TMPDIR/$TMPNAME-$(printf "%04g" $INDEX).pdf "$1"
-
-    ((INDEX++))
-done
+# split input.pdf
+split() {
+	mkdir $TMPDIR
+	INDEX=1
+	# start loop from 3rd argument
+	for RANGE in $PAGES
+	do
+	    START=$(echo $RANGE | cut -d'-' -f1)
+	    END=$(echo $RANGE | cut -d'-' -f2)
+	
+	    # create new pdf from range $START-$END
+	    #'$(printf "%04g" $INDEX)' = padding $INDEX with leading zeros
+	    gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=$START -dLastPage=$END -sOutputFile=$TMPDIR/$TMPNAME-$(printf "%04g" $INDEX).pdf "$INPUT_FILE"
+	
+	    ((INDEX++))
+	done
+}
 
 # merge all 'temp-pdf-snapshot' files into one output.pdf
-sh $SCRIPT_DIR/merge-pdf.sh $2 $TMPDIR/$TMPNAME*.pdf
+merge() {
+	sh $SCRIPT_DIR/merge-pdf.sh $OUTPUT_FILE $TMPDIR/$TMPNAME*.pdf
+}
 
-#rm -rf $TMPDIR
+# main()
+split
+if [ $? -eq 0 ]; then merge; fi
+rm -rf $TMPDIR
+
