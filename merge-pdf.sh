8c70225b8de6bcb10f08defa6905554be11e9cfc
mrl5 <jakuboo+github@gmail.com>
2018-09-25 09:50:08 +0200

    check if output.pdf exists

--- merge-pdf.sh
+++ merge-pdf.sh
@@ -19,39 +19,69 @@
 # Example 2: 'sh merge-pdf.sh /path/to/ouotput.pdf /path/to/input-*.pdf'
 #  creates output.pdf from files matching pattern input-*.pdf
 
-arg_err_msg="Expected minimum 2 arguments. Please specify output file and input files."
-
 # checks if enough arguments were provided
 if [ $# -lt 2 ]; then
-	echo "$arg_err_msg"
+	echo "Expected minimum 2 arguments. Please specify output file and input files."
 	exit 1
 else
+	OUTPUT="$1"
 	# stores args $2, $3, $4, ..., $n
-	INPUT_GROUP=${@:2}
+	INPUT_GROUP="${@:2}"
 fi
 
 # checks if output file exists
 check_output() {
-	
+	ow_msg="$OUTPUT already exists. Do you want to overwrite it? [yes/no]: "
+	abort_msg="Aborting."
+
+	if [ -f "$OUTPUT" ]; then
+		# ask to overwrite
+		read -p "$ow_msg" answr
+		
+		while [[ -z $answr ]]; do
+			answr="yes"
+		done
+
+		# Convert $answr to lower cases
+		ovrwrt=${answr,,}
+		case $ovrwrt in
+			no)
+				echo $abort_msg
+				exit 1
+				;;
+			n)
+				echo $abort_msg
+				exit 1
+				;;
+			yes)
+				;;
+			y)
+				;;
+			*)
+				echo "Wrong answer."
+				check_output
+				;;
+		esac
+	fi
 }
 
 # merges multiple .pdf files into one using GhostScript
 merge() {
-	echo Creating "$1" ...
+	echo Creating $OUTPUT ...
 	echo ""
-	gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE="$1" -dBATCH $INPUT_GROUP
+	gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE="$OUTPUT" -dBATCH $INPUT_GROUP
 	# checks if last command returned error
 	if [ $? -eq 0 ]; then
 		echo ""
-		echo Done. New PDF: "$1"
+		echo Done. New PDF: \'$OUTPUT\'
 	else
 		echo ""
-		echo Something went wrong. "$1" may be corrupted.
+		echo Something went wrong. \'$OUTPUT\' may be corrupted.
 	fi
 }
 
-# real work starts here
-#check_output
+# main()
+check_output
 #check_inputs
 merge
 
